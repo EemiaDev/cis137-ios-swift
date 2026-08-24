1cf07a09a1a441343029afe95e7387dbc90237a8
Aimee Jin <214427659+EemiaDev@users.noreply.github.com>
2026-08-24 00:26:44 -0700

    Initial Hello project

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Aimee Jin on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
