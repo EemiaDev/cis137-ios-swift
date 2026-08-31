451485d7e67f7f1c9f83dfc26167b794c5de81a5
Aimee Jin <214427659+EemiaDev@users.noreply.github.com>
2026-08-30 23:22:18 -0700

    Add Assignment 2 playground

--- Assignment2.playground/Contents.swift
+++ Assignment2.playground/Contents.swift
@@ -1,3 +1,27 @@
+/*:
+ Homework 2
+ 
+ Name: Aimee Jin
+ 
+ Date: August 30, 2026
+ */
+
 import UIKit
 
-var greeting = "Hello, playground"
+let names = ["Anna": 25, "Alex": 36, "Brian": 47, "Jack": 16, "Bob": 69]
+let greetings = ["Hello", "Hi", "Good morning", "Good afternoon", "Good evening"]
+
+for (name, age) in names {
+    let randomIndex = Int.random(in: 0..<5)
+    print("\(greetings[randomIndex]), \(name)! Happy \(age)th birthday!")
+    switch age {
+    case 0..<18:
+        print("You are still a child.")
+    case 18...25:
+        print("You are a young adult.")
+    case 26...65:
+        print("You are an adult.")
+    default:
+        print("You are a senior.")
+    }
+}
