44d172eab6eb9228eda3d9554f508e795882b2f1
Aimee Jin <214427659+EemiaDev@users.noreply.github.com>
2026-09-22 18:17:00 -0700

    add assignment 5

---
+++ Assignment5/Assignment5/Assignment5App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment5App.swift
+//  Assignment5
+//
+//  Created by Aimee Jin on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Assignment5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
